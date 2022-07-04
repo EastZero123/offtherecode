496ed5dacddbb4ac9a36778e7900762d77f0bbb4
EastZero123 <99165555+EastZero123@users.noreply.github.com>
2022-07-04 14:12:50 +0900

    Add files via upload

---
+++ teamProject_copy/src/App.js
@@ -0,0 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import React from "react";
+import "./App.css";
+import Home from "./Main/Home";
+import Login from "./Login/Login";
+import Signup from "./Login/Signup";
+import Mypage from "./Login/Mypage";
+import Board from "./Board/Board";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/mypage" element={<Mypage />} />
+        <Route path="/board" element={<Board />} />
+      </Routes>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ teamProject_copy/src/Layout/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <React.Fragment>
+      <h1>헤더입니다</h1>
+    </React.Fragment>
+  );
+};
+
+export default Header;
